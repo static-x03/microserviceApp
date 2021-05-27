3512e7ba9684f1a0c2c6fee6a1b3e71b8e069c87
static-x03 <static-x03@hotmail.com>
2021-05-27 16:28:32 -0500

    Agregando Registry-Service

---
+++ registry-service/src/test/java/com/bvanegas/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bvanegas.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
